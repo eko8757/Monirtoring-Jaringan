5e492fd7bdb040f6863038725090a89bce465ce6
andika octavia pratama putra <andhika@actdisain.com>
2019-07-21 19:47:30 +0700

    [ADD] data dummy graphView bandwidth information [ADD] data dummy graphView network information

--- app/src/main/java/com/skripsi/monitorjaringan/BandwidthUsage.kt
+++ app/src/main/java/com/skripsi/monitorjaringan/BandwidthUsage.kt
@@ -2,20 +2,24 @@ package com.skripsi.monitorjaringan
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
-import com.jjoe64.graphview.series.DataPointInterface
 import com.jjoe64.graphview.series.LineGraphSeries
-import kotlinx.android.synthetic.main.activity_bandwidth_usage.*
-
 
 class BandwidthUsage : AppCompatActivity() {
 
+    lateinit var graphView: GraphView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bandwidth_usage)
 
-
+        graphView = findViewById(R.id.graph_bandwidth)
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(arrayOf(
+            DataPoint(0.toDouble(), 7.toDouble()),
+            DataPoint(2.toDouble(), 4.toDouble()),
+            DataPoint(6.toDouble(), 9.toDouble())
+        ))
+        graphView.addSeries(series)
     }
-
 }
--- app/src/main/java/com/skripsi/monitorjaringan/NetworkInformation.kt
+++ app/src/main/java/com/skripsi/monitorjaringan/NetworkInformation.kt
@@ -3,15 +3,23 @@ package com.skripsi.monitorjaringan
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 
 class NetworkInformation : AppCompatActivity() {
 
-    lateinit var graph: GraphView
+    lateinit var graphView: GraphView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network_information)
+
+        graphView = findViewById(R.id.graph_network)
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(arrayOf(
+            DataPoint(0.toDouble(), 7.toDouble()),
+            DataPoint(2.toDouble(), 4.toDouble()),
+            DataPoint(6.toDouble(), 9.toDouble())
+        ))
+        graphView.addSeries(series)
     }
-
-
 }
